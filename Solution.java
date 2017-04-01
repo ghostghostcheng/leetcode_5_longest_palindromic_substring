72375658a7558a63531c92b687da2c9f4468efc5
Charlene Cheng <chsmile0310@gmail.com>
2017-04-02 04:55:56 +0800

    Update Solution.java
    refactor: coding style

--- Solution.java
+++ Solution.java
@@ -4,22 +4,22 @@ public class Solution {
     public static String longestPalindrome(String s) {
     	String resultStr = "";
     	for (int i = 0; i < s.length(); i++) {
-    		int tailIdx = s.length() - 1, counter = 0, j = s.length() - 1;
-    		for (; j > -1 && tailIdx > -1;) {
-    		    if (resultStr.length() > (j - i + 1)) {
-    		    	break;
-    		    }
-    		    else if (tailIdx - i >= 0 && s.charAt(i + counter) == s.charAt(tailIdx)) {
-					tailIdx = (counter != 0) ? tailIdx : j;
-					resultStr = (tailIdx - i != 0) ? resultStr : s.substring(i, j + 1);
-					counter = (tailIdx - i != 0) ? ++counter : 0;
-    				tailIdx--;
-    				continue;
-    			} else {
-        			counter = 0;
-    			    tailIdx = --j;
-    			}
-    		}
+		int tailIdx = s.length() - 1, counter = 0, j = s.length() - 1;
+		for (; j > -1 && tailIdx > -1;) {
+			if (resultStr.length() > (j - i + 1)) {
+				break;
+			}
+			else if (tailIdx - i >= 0 && s.charAt(i + counter) == s.charAt(tailIdx)) {
+				tailIdx = (counter != 0) ? tailIdx : j;
+				resultStr = (tailIdx - i != 0) ? resultStr : s.substring(i, j + 1);
+				counter = (tailIdx - i != 0) ? ++counter : 0;
+				tailIdx--;
+				continue;
+			} else {
+				counter = 0;
+				tailIdx = --j;
+			}
+		}
     	}
     	return resultStr;
     }  
