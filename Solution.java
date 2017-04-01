2b1acd37eaaef34bea96dc224afc7570e473077c
ghostghostcheng <charlene_110@yahoo.com.tw>
2017-04-02 04:52:54 +0800

    fix: optimize code

--- Solution.java
+++ Solution.java
@@ -2,42 +2,25 @@ package leetcode_5_longest_palindromic_substring;
 
 public class Solution {
     public static String longestPalindrome(String s) {
-        if (s.length() < 2) {
-            return s;
-        }
-        String tmpStr = "";
-        String resultStr = s.charAt(0) + "";
-        
-        for (int i = 0; i < s.length(); i++) {
-            char tmpChar = s.charAt(i);
-            if (i == s.length() - 1) {
-                break;
-            }
-            
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(i) == tmpChar) {
-                    tmpStr = s.substring(i, j + 1);
-                    
-                    if (checkPalindromic(tmpStr)) {
-                        if (tmpStr.length() > resultStr.length()) {
-                            resultStr = tmpStr;
-                        }    
-                    }
-                }
-            }
-        }
-        return resultStr;
-    }
-    
-    public static boolean checkPalindromic(String str) {
-        String tmpStr = "";
-        for (int i = str.length() - 1; i > 0; i--) {
-            tmpStr = tmpStr + str.charAt(i);
-        }
-        tmpStr = tmpStr + str.charAt(0);
-        if (tmpStr.equals(str)) {
-            return true;
-        }
-        return false;
-    }
+    	String resultStr = "";
+    	for (int i = 0; i < s.length(); i++) {
+    		int tailIdx = s.length() - 1, counter = 0, j = s.length() - 1;
+    		for (; j > -1 && tailIdx > -1;) {
+    		    if (resultStr.length() > (j - i + 1)) {
+    		    	break;
+    		    }
+    		    else if (tailIdx - i >= 0 && s.charAt(i + counter) == s.charAt(tailIdx)) {
+					tailIdx = (counter != 0) ? tailIdx : j;
+					resultStr = (tailIdx - i != 0) ? resultStr : s.substring(i, j + 1);
+					counter = (tailIdx - i != 0) ? ++counter : 0;
+    				tailIdx--;
+    				continue;
+    			} else {
+        			counter = 0;
+    			    tailIdx = --j;
+    			}
+    		}
+    	}
+    	return resultStr;
+    }  
 }
